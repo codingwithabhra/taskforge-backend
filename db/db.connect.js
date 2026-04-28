340729c1bdc9dd7ede0cf8a99e2f95dcdceba41d
abhra <patra.abhra97@gmail.com>
2026-04-28 15:22:20 +0530

    connection string code updated

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,14 +1,16 @@
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-const mongouri = process.env.MONGODB;
+const mongoUri = process.env.MONGODB;
 
-const initialisedatabase = async() => {
-    await mongoose.connect(mongouri).then(()=>{
-        console.log("Connected to database");
-    }).catch((error)=>{
-        console.log("Error connecting to database", error);
-    });
+const initializeDatabase = async () => {
+  try {
+    await mongoose.connect(mongoUri);
+    console.log("Connected to database");
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+    process.exit(1);
+  }
 };
 
-module.exports = { initialisedatabase };
+module.exports = { initializeDatabase };
